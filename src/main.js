112685e18165da9c0920e9eb2c8f2731d46cff75
Aaron Jeong <jimmy53120488@gmail.com>
2025-04-06 21:32:41 -0400

    feat: add axesHelper and wireframe

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 const scene = new THREE.Scene();
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "violet" });
+const cubeMaterial = new THREE.MeshBasicMaterial({
+	color: "violet",
+	wireframe: true,
+});
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
 scene.add(cubeMesh);
+cubeMesh.add(new THREE.AxesHelper(2));
 
 const camera = new THREE.PerspectiveCamera(
 	50,
@@ -15,17 +19,6 @@ const camera = new THREE.PerspectiveCamera(
 	0.1,
 	200,
 );
-
-// const aspectRatio = window.innerWidth / window.innerHeight;
-
-// const camera = new THREE.OrthographicCamera(
-// 	-1 * aspectRatio,
-// 	1 * aspectRatio,
-// 	1,
-// 	-1,
-// 	0.1,
-// 	200,
-// );
 
 camera.position.z = 5;
 const canvas = document.getElementById("threejs-canvas");
